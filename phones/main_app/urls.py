096e452aaf0928425949839c59730a51eabf340d
Richard Clarke <freshnewrich@gmail.com>
2022-07-12 16:24:02 -0700

    comment urls.py

--- phones/main_app/urls.py
+++ phones/main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+#path function used to define each route
 from . import views
+# . represents current directory
+# views represents views.py model
 
 urlpatterns = [
-    
+
 ]
