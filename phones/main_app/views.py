312ed5211cc009113183028bfcebdb00a8314449
Richard Clarke <freshnewrich@gmail.com>
2022-07-12 16:32:15 -0700

    add about view

--- phones/main_app/views.py
+++ phones/main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 #define home view
 def home(request):
     return HttpResponse('HAVE SOME OF THESE <a href="/phones">PHONES!</a>')
+
+def about(request):
+    return HttpResponse("what are you all about?")
