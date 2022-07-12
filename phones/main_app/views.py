45b0591ae78e7bfd8d0565bcd44d865caa63f9e3
Richard Clarke <freshnewrich@gmail.com>
2022-07-12 16:33:34 -0700

    add about link to / view

--- phones/main_app/views.py
+++ phones/main_app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 #define home view
 def home(request):
-    return HttpResponse('HAVE SOME OF THESE <a href="/phones">PHONES!</a>')
+    return HttpResponse('HAVE SOME OF THESE <a href="/phones">PHONES!</a> and have some <a href="/about">about</a> as well.')
 
 def about(request):
     return HttpResponse("what are you all about?")
