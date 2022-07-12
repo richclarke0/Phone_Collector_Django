ddc167cc3be0511d619afbc86090420226ddc1f3
Richard Clarke <freshnewrich@gmail.com>
2022-07-12 16:51:58 -0700

    mod views.py to render the templated index page instead

--- phones/main_app/views.py
+++ phones/main_app/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 #define home view
 def home(request):
-    return HttpResponse('HAVE SOME OF THESE <a href="/phones">PHONES!</a> and have some <a href="/about">about</a> as well.')
-
+    # return HttpResponse('HAVE SOME OF THESE <a href="/phones">PHONES!</a> and have some <a href="/about">about</a> as well.')
+    return render(request, 'catchpage.html')
 # def about(request):
 #     return HttpResponse("what are you all about?")
 def about(request):
