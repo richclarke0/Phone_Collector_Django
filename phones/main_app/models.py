8854284bc611e4bf9ac62ad52ebaf62d7280dbd1
Richard Clarke <freshnewrich@gmail.com>
2022-07-15 16:35:32 -0700

    remove Phone from views, put in Models

--- phones/main_app/models.py
+++ phones/main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    img = models.CharField(max_length=500)
