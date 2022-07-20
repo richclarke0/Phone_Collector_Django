7e1d576cfa06e2fdcae84c6e8176af7bd98c51d6
Richard Clarke <freshnewrich@gmail.com>
2022-07-19 18:01:05 -0700

    typo

--- phones/main_app/views.py
+++ phones/main_app/views.py
@@ -13,7 +13,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.control.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 import uuid
 import boto3
